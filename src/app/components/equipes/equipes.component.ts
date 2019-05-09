3e70d9d922f3e7d8b9ea56ef9059d8d4f049b9fb
Gabriel Todesco <gabriel.todesco97@gmail.com>
2019-05-09 20:03:18 -0300

    Pega as informações do usuário ao entrar na tela de Equipes

--- src/app/components/equipes/equipes.component.ts
+++ src/app/components/equipes/equipes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { PessoaService } from '../../services/pessoa.service';
 
 @Component({
   selector: 'app-equipes',
@@ -9,14 +10,37 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class EquipesComponent implements OnInit {
 
   isScrumMaster = true; // flag para testes de ScrumMaster
+  nome;
+  equipe;
 
   equipes: string[] = ['Fênix', 'Monster', 'Transformers', 'CuboD4', 'Anônima'];
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, 
+              private router: Router, 
+              private pessoaService: PessoaService) { }
 
   ngOnInit() {
 
+    this.pessoaService.getPessoaByEmail(sessionStorage.getItem("email"))
+    .subscribe(
+      res=>{
+        let pessoa = this.getOjbJSON(res);
+        this.nome = pessoa.nome.split(" ", 1); // Primeiro nome apenas
+        this.isScrumMaster = pessoa.scrum_Master;
+
+        if(!this.isScrumMaster){ // Se não for Scrum Master
+          this.equipe = pessoa.equipe;
+        }
+
+        console.log(res);
+      },
+      error => console.log(error)
+    );
     
+  }
+
+  getOjbJSON(json: any){
+    return JSON.parse(JSON.stringify(json));
   }
 
   criarEquipe(){
