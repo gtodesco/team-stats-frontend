493f94eaa452adff1d5176de2d06009b04bc84f2
Gabriel Todesco <gabriel.todesco97@gmail.com>
2019-06-18 18:56:28 -0300

    Ações para criar e encerrar sprint

--- src/app/components/sprints/sprints.component.ts
+++ src/app/components/sprints/sprints.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PessoaService } from '../../services/pessoa.service';
 import { EquipeService } from '../../services/equipe.service';
+import { SprintService } from '../../services/sprint.service';
 
 @Component({
   selector: 'app-sprints',
@@ -13,11 +14,13 @@ export class SprintsComponent implements OnInit {
   isScrumMaster = true; // flag para controle se é Scrum Master
   equipe = {};
   sprints = [];
+  sprintAtual;
 
   constructor(private route: ActivatedRoute, 
               private router: Router, 
               private pessoaService: PessoaService,
-              private equipeService: EquipeService) { }
+              private equipeService: EquipeService,
+              private sprintService: SprintService) { }
 
   ngOnInit() {
 
@@ -37,8 +40,11 @@ export class SprintsComponent implements OnInit {
         this.sprints = this.getOjbJSON(res).sprints;
 
         for(var i = 0; i <= this.sprints.length -1; i++){
-          this.sprints[i].pontConcluidas = parseFloat(this.sprints[i].pontConcluidas);
-          this.sprints[i].qtdDias = parseFloat(this.sprints[i].qtdDias);
+          this.sprints[i].pontConcluidas = Number(this.sprints[i].pontConcluidas);
+          this.sprints[i].qtdDias = Number(this.sprints[i].qtdDias);
+          if(this.sprints[i].finalizada == false){
+            this.sprintAtual = this.sprints[i];
+          }
         }
 
         //debugger;
@@ -48,6 +54,33 @@ export class SprintsComponent implements OnInit {
     );
 
 
+  }
+
+  criarSprint(){
+    console.log("criando sprint");
+  }
+
+  encerrarSprint(){
+    console.log("encerrar sprint");
+
+    if(this.sprintAtual != null){
+      this.sprintAtual.finalizada = true;
+
+      this.sprintService.putSprint(this.sprintAtual).subscribe(
+        res => {
+            
+          location.reload();
+          
+        },
+        error => console.log(error)
+      
+      );
+    }
+  }
+
+  voltar(){
+    sessionStorage.removeItem("equipe");
+    this.router.navigate(['/equipes']);
   }
 
   getOjbJSON(json: any){
